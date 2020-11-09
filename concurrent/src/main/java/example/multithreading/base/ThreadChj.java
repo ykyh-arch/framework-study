89385af494240f3daf05cb0b0b72b9a6975f1710
guoyukai <yukai.guo@uetec.cn>
2020-11-09 14:41:16 +0800

    项目初始化；

---
+++ concurrent/src/main/java/example/multithreading/base/ThreadChj.java
@@ -0,0 +1,35 @@
+package example.multithreading.base;
+
+/**
+ * @ClassName: ThreadChj
+ * @Description: 线程创建
+ * @Author: Uetec
+ * @Date: 2020-11-02-11:23
+ * @Version: 1.0
+ **/
+public class ThreadChj{
+
+    public static void main(String[] args) {
+        Thread1 thread1 = new Thread1();
+        thread1.start();
+
+        new Thread(new Thread2()).start();
+    }
+}
+//方式一：继承Thread
+class Thread1 extends Thread{
+
+    @Override
+    public void run() {
+        System.out.println("线程名:"+Thread.currentThread().getName());
+    }
+
+}
+//方式二：：实现Runnable
+class Thread2 implements Runnable{
+
+    public void run() {
+        System.out.println("线程名:"+Thread.currentThread().getName());
+    }
+}
+
