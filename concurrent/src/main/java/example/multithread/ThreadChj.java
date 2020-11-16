2b7944d0094dfe3f81b93987ed613338c3a6469b
guoyukai <yukai.guo@uetec.cn>
2020-11-16 17:46:17 +0800

    线程池；

--- concurrent/src/main/java/example/multithread/ThreadChj.java
+++ concurrent/src/main/java/example/multithread/ThreadChj.java
@@ -1,5 +1,8 @@
 package example.multithread;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
 /**
  * @ClassName: ThreadChj
  * @Description: 线程创建
@@ -9,11 +12,22 @@ package example.multithread;
  **/
 public class ThreadChj{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Thread1 thread1 = new Thread1();
         thread1.start();
 
         new Thread(new Thread2()).start();
+
+        //创建FutureTask的对象
+        FutureTask<String> task = new FutureTask<String>(new Thread3());
+        //创建Thread类的对象
+        Thread thread3 = new Thread(task,"thread-3");
+        //开启线程
+        thread3.start();
+        //获取call()方法的返回值，即线程运行结束后的返回值
+        String result = task.get();
+        System.out.println(result);
+
     }
 }
 //方式一：继承Thread
@@ -30,6 +44,16 @@ class Thread2 implements Runnable{
 
     public void run() {
         System.out.println("线程名:"+Thread.currentThread().getName());
+    }
+}
+
+//方式三：：实现Callable
+class Thread3 implements Callable<String> {
+
+    @Override
+    public String call() throws Exception {
+        System.out.println("线程名:"+Thread.currentThread().getName());
+        return "线程名:"+Thread.currentThread().getName();
     }
 }
 
